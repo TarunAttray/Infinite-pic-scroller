3e70ae508f0ef269deccd9c943503993b4a6e53f
Tarunn Sharmaa <36966951+TarunnSharmaa@users.noreply.github.com>
2022-03-31 02:39:30 +0530

    Update script.js

--- Project-1 Infinity Scroller/script.js
+++ Project-1 Infinity Scroller/script.js
@@ -1,12 +1,12 @@
 const imageContainer=document.getElementById('image-container');
 const loader=document.getElementById('loader');
 
-let photosArray=[];
+
 
 
 //unsplash api
 const count=10;
-const apikey='x2bSjwymCSe3VTl5N64cfmd0GmfFWemqMQ28j0Hicws';
+const apikey='-QBHUuVwvvegNaAQIBLL8KEPDo-XlpOyaGBeRcfJJHc';
 const apiUrl=`https://api.unsplash.com/photos/random?client_id=${apikey}&count=${count}`;
 
 // Helper Function to Set Attributes on DOM Elements
@@ -16,13 +16,13 @@ function setAttribute(element, attributes) {
     }
   }
   
-
+  let photosArray=[];
 
 // crete elements for links & photos, add to dom
 function displayPhotos(){
     photosArray.forEach((photo)=>{
         //create <a> to link to unsplash
-        const item =document.createElement('a');
+        const item = document.createElement('a');
         setAttribute(item,{
             href: photo.links.html,
             target: '_blank',
@@ -34,7 +34,6 @@ function displayPhotos(){
             src:photo.urls.regular,
             alt:photo.alt_description,
             title:photo.alt_description,
-            
         });
         // put <img> inside <a> . then put both of them in image container.
         item.appendChild(img);  
@@ -48,11 +47,8 @@ function displayPhotos(){
 async function getPhotos(){
     try{
         const response=await fetch(apiUrl);
-        
-        photosArray = await response.json();
+        photosArray =   await response.json();
         displayPhotos();
-       
-
     }catch(error){
 //catch error here
 console.log(error);
